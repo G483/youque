824adf66712e9fea99f047f5fa357b4117c6fa46
G483 <gabrijel.web@gmail.com>
2018-04-03 16:15:44 +0200

    scetch a basic layout with colors

--- server.js
+++ server.js
@@ -10,4 +10,9 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname+'/index.html'))
 })
 
+app.get('/mockup', (req, res) => {
+  res.contentType('text/html')
+  res.sendFile(path.join(__dirname+'/mockup.html'))
+})
+
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
